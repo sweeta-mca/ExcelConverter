eb10aa56b3d0a3bd572a097d1666bff5e6ebb8a3
sweeta2022 <lourdupaulet.sweeta@stratos.studio>
2022-07-13 15:49:30 +0530

    Home page and controller code added

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for,send_from_directory
+from flask import Flask, flash, request, redirect, url_for,send_from_directory,render_template
 from werkzeug.utils import secure_filename
 from convert import Converter
 
@@ -27,58 +27,73 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# @app.route('/encoded_download', methods=['GET', 'POST'])
+# def download_file():
+#     if request.method == 'POST':            
+#         filename = request.form['filename']                
+#         if filename == '':
+#             flash('No file name given')
+#             return redirect(request.url)  
+#         if filename and allowed_file(filename):  
+#             source_path = os.path.join(app.config['ENCODED_FOLDER'], filename)
+#             destination_path = os.path.join(app.config['DECODED_FOLDER'], filename)
+#             con.toBase64(source_file_path=source_path,destination_file_path=destination_path)
+#             return send_from_directory(app.config['DECODED_FOLDER'], filename)
+#         return render_template('home.html')
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # If the user does not select a file, the browser submits an
+#         # empty file without a filename.
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             source_path =os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#             destination_path = os.path.join(app.config['ENCODED_FOLDER'], filename)
+#             con.toBase64(source_file_path=source_path,destination_file_path=destination_path)
+#             #return redirect(url_for('download', name=filename))
+#             return "File encoded successfully"
+#     return render_template('home.html')
+
+
 @app.route('/download', methods=['GET', 'POST'])
-def download_file():
-    if request.method == 'POST':            
-        filename = request.form['filename']                
-        if filename == '':
+def download_file_encode_decode():
+    if request.method == 'POST':    
+        file = request.files['file']
+        if file.filename == '':
             flash('No file name given')
             return redirect(request.url)  
-        if filename and allowed_file(filename):  
-            source_path = os.path.join(app.config['ENCODED_FOLDER'], filename)
-            destination_path = os.path.join(app.config['DECODED_FOLDER'], filename)
-            con.toBase64(source_file_path=source_path,destination_file_path=destination_path)
-            return send_from_directory(app.config['DECODED_FOLDER'], filename)
-    return '''
-    <!doctype html>
-    <title>Download a File</title>
-    <h1>Download a File</h1>
-    <form method=post action="/download">
-       <input type="text" name="filename"><br>
-      <input type=submit value=download>
-    </form>
-    '''
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            source_path =os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        if file and allowed_file(file.filename) == False:  
+            flash('No file name given')
+            return redirect(request.url)  
+        
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #---Encode--
+        if (request.form['encode_decode'] == "Encode"):
+            source_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             destination_path = os.path.join(app.config['ENCODED_FOLDER'], filename)
             con.toBase64(source_file_path=source_path,destination_file_path=destination_path)
-            #return redirect(url_for('download', name=filename))
-            return "File encoded successfully"
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+            return send_from_directory(app.config['ENCODED_FOLDER'], filename)
+            
+        #---Decode--
+        elif (request.form['encode_decode'] == "Decode"):
+            source_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            destination_path = os.path.join(app.config['DECODED_FOLDER'], filename)
+            con.fromBase64(source_file_path=source_path,destination_file_path=destination_path)
+            return send_from_directory(app.config['DECODED_FOLDER'], filename)
+        
+    return render_template('home.html')
+
 
 
 if __name__ == "__main__":
